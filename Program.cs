41454e79e1f1222749015f03efbb2ae493efd112
Thang Phan <phanthq2k2@gmail.com>
2025-01-22 16:22:05 +0700

    - delete range in an array - input have some problems when -> A[]

--- Program.cs
+++ Program.cs
@@ -5,34 +5,63 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Console.WriteLine("Nhap size cua mang:");
+        int size = int.Parse(Console.ReadLine());
 
-        string[] names = {
-            "Nguyễn  vAn Thanh",
-            " trần   thị Nhung.",
-            "Huỳnh Thúc Điền.",
-            "Lê van  NaM"
-        };
+        int[] A = new int[size];
+        Console.WriteLine("Nhap cac phan tu cua mang:");
+        string[] input = Console.ReadLine().Split(' ');
 
-        Console.WriteLine("Data Dep:");
-        foreach (var name in names)
+        for (int i = 0; i < size; i++)
         {
-            Console.WriteLine(FormatName(name));
+            A[i] = int.Parse(input[i]);
         }
+
+        Console.WriteLine("Nhap fromIndex:");
+        int fromIndex = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Nhap toIndex:");
+        int toIndex = int.Parse(Console.ReadLine());
+
+        int[] result = DeleteRangeInArray(A, fromIndex, toIndex);
+
+        Console.WriteLine("Result:");
+        Console.WriteLine(string.Join(", ", result));
+
+    }
+    public static int[] DeleteRangeInArray(int[] A, int fromIndex, int toIndex)
+    {
+        if (A == null || fromIndex < 0 || toIndex >= A.Length || fromIndex > toIndex)
+        {
+            Console.WriteLine("Input bi loi!");
+            return [];
+        }
+
+        for (int i = fromIndex; i <= toIndex; i++)
+        {
+            A = DeleteIndexInArray(A, fromIndex);
+        }
+
+        return A;
     }
 
-    public static string FormatName(string name)
+    public static int[] DeleteIndexInArray(int[] A, int index)
     {
-        if (string.IsNullOrWhiteSpace(name))
-            return "--Ten Loi--";
+        if (A == null || index < 0 || index >= A.Length)
+        {
+            Console.WriteLine("Index bi loi!");
+            return [];
+        }
 
-        name = Regex.Replace(name, @"[^\p{L}\s]", "");
+        int[] result = new int[A.Length - 1];
+        for (int i = 0, j = 0; i < A.Length; i++)
+        {
+            if (i != index)
+            {
+                result[j++] = A[i];
+            }
+        }
 
-        var formattedName = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(
-            name.ToLower().Trim()
-                .Replace("  ", " ")
-        );
-
-        return formattedName;
+        return result;
     }
 }
