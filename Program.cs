d2d53217f2d667ff681673b0034f240f7f935cd5
Thang Phan <phanthq2k2@gmail.com>
2025-01-22 12:01:31 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Nhap size cua array:");
+        int size = int.Parse(Console.ReadLine());
+        if (size <= 0)
+            return;
+        int[] a = new int[size];
+
+        Console.WriteLine("Nhap phan tu cua array:");
+        for (int i = 0; i < a.Length; i++)
+        {
+            Console.WriteLine($"Phan tu thu {i + 1}:");
+            a[i] = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine("Nhap phan tu can tim:");
+        int n = int.Parse(Console.ReadLine());
+
+        bool result = CheckNumInArray(a, size, n);
+        Console.WriteLine(result);
+    }
+    public static bool CheckNumInArray(int[] A, int size, int n)
+    {
+        for (int i = 0; i < size; i++)
+        {
+            if (A[i] == n)
+                return true;
+        }
+        return false;
+    }
+}
