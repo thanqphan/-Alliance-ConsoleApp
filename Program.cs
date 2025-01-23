a75940afa2ef99555efe2f9c55d48709dadadefd
Thang Phan <phanthq2k2@gmail.com>
2025-01-23 13:28:47 +0700

    - find image files

--- Program.cs
+++ Program.cs
@@ -1,67 +1,37 @@
-﻿using System.Globalization;
-using System.Text.RegularExpressions;
-
-class Program
+﻿class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Nhap size cua mang:");
-        int size = int.Parse(Console.ReadLine());
+        Console.WriteLine("Nhap path cua folder:");
+        string pathOfFolder = Console.ReadLine();
 
-        int[] A = new int[size];
-        Console.WriteLine("Nhap cac phan tu cua mang:");
-        string[] input = Console.ReadLine().Split(' ');
-
-        for (int i = 0; i < size; i++)
+        List<string> imageFiles = GetImageFiles(pathOfFolder);
+        Console.WriteLine("Danh sach File Anh:");
+        foreach (var file in imageFiles)
         {
-            A[i] = int.Parse(input[i]);
+            Console.WriteLine(file);
         }
-
-        Console.WriteLine("Nhap fromIndex:");
-        int fromIndex = int.Parse(Console.ReadLine());
-
-        Console.WriteLine("Nhap toIndex:");
-        int toIndex = int.Parse(Console.ReadLine());
-
-        int[] result = DeleteRangeInArray(A, fromIndex, toIndex);
-
-        Console.WriteLine("Result:");
-        Console.WriteLine(string.Join(", ", result));
 
     }
-    public static int[] DeleteRangeInArray(int[] A, int fromIndex, int toIndex)
+    public static List<string> GetImageFiles(string pathOfFolder)
     {
-        if (A == null || fromIndex < 0 || toIndex >= A.Length || fromIndex > toIndex)
+        if (string.IsNullOrEmpty(pathOfFolder) || !Directory.Exists(pathOfFolder))
         {
-            Console.WriteLine("Input bi loi!");
-            return [];
+            Console.WriteLine("Path folder khong hop le - khong ton tai!");
         }
 
-        for (int i = fromIndex; i <= toIndex; i++)
+        string[] imageExtensions = { ".jpg", ".jpeg", ".png", ".bmp", ".gif", ".tiff", ".webp" };
+
+        List<string> imageFiles = new List<string>();
+        string[] allFiles = Directory.GetFiles(pathOfFolder, "*.*", SearchOption.AllDirectories);
+
+        foreach (var file in allFiles)
         {
-            A = DeleteIndexInArray(A, fromIndex);
+            string extension = Path.GetExtension(file).ToLower();
+            if (Array.Exists(imageExtensions, ext => ext == extension))
+                imageFiles.Add(file);
         }
 
-        return A;
-    }
-
-    public static int[] DeleteIndexInArray(int[] A, int index)
-    {
-        if (A == null || index < 0 || index >= A.Length)
-        {
-            Console.WriteLine("Index bi loi!");
-            return [];
-        }
-
-        int[] result = new int[A.Length - 1];
-        for (int i = 0, j = 0; i < A.Length; i++)
-        {
-            if (i != index)
-            {
-                result[j++] = A[i];
-            }
-        }
-
-        return result;
+        return imageFiles;
     }
 }
